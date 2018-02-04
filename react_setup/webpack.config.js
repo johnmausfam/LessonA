62d151524b250fa8b39693d3a376af0d22f0fc8c
fam <fam1923@gmail.com>
2018-02-04 22:26:05 +0800

    init react_setup

---
+++ react_setup/webpack.config.js
@@ -0,0 +1,30 @@
+const path = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+    entry: {
+        bundle: [
+            'react-hot-loader/patch',
+            'webpack-dev-server/client?http://localhost:3000/', /*包進去就不用在index.html中引用 */
+            'webpack/hot/dev-server', /* HOT熱更新模組 */
+            path.resolve(__dirname, 'src/index.jsx')
+        ]
+    },
+    output: {
+        filename: '[name].js',
+        publicPath: '/' /*build好的entry的JS會在這邊,因為react-hot-loader限制必須為"/" */
+    },
+    plugins: [
+        new webpack.HotModuleReplacementPlugin(), /*HOT熱更新模組插件*/
+        new webpack.NamedModulesPlugin(), /* 更新時可以看到更新的檔案名稱*/
+        new webpack.SourceMapDevToolPlugin() /*可以在F12看到原始碼*/
+    ],
+    module: {
+        rules: [
+            { test: /\.(js|jsx)$/, exclude: /node_modules/, loader: "babel-loader" }
+        ]
+    },
+    resolve: {
+        extensions: [".js", ".json", ".jsx"] /*import可不加附檔名*/
+    }
+};
